b827ce0f33519b753aca0e066180a86c960dc57c
Mostly Void <7rat13@gmail.com>
2022-12-06 02:14:22 +0530

    Correctly redirect when using bangs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,20 @@ async function handleRequest(req) {
   const query = url.searchParams.get("q");
   // const res = await fetch(`https://lite.duckduckgo.com/lite?q=${query}`);
   // const res = await fetch(`https://www.google.com/search?q=${query}`);
-  const res = await fetch(`https://search.brave.com/search?q=${query}`);
+
+  // TODO can we store the list of bangs using KV or cloudflare d1?
+  // and retrive them faster than ddg's redirection
+  const braveSearchUrl = `https://search.brave.com/search?q=${query}`;
+  // if query contains a duckdugo bang, just directly use brave
+  // to search for it, the bang can be anywhere in the query
+  // e.g. "!g hello world" or "hello !g world" or "hello !amazon books"
+  // the bang can contain numbers and letters
+
+  if (query?.match(/!([a-z0-9]+)/)) {
+    return Response.redirect(braveSearchUrl, 301);
+  }
+
+  const res = await fetch(braveSearchUrl);
 
   const contentType = res.headers.get("Content-Type");
 
